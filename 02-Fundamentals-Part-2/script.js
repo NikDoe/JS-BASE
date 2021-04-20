b096dd732cbfcae982f075a4cfeb0841916c5a35
NikDoe <nikd0e@yandex.ru>
2021-04-20 17:10:58 +0300

    Arrow functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -35,7 +35,6 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
-*/
 
 ///////////////////////////////////////
 // Function Declarations vs. Expressions
@@ -53,3 +52,21 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = (birthYeah) => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
