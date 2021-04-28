d86e51db122f6ad58904554d59ab30a392d1179e
NikDoe <nikd0e@yandex.ru>
2021-04-28 17:58:44 +0300

    The Nullish Coalescing Operator

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -50,6 +50,17 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+///////////////////////////////////////
 // Short Circuiting (&& and ||)
 
 console.log('---- OR ----');
@@ -81,7 +92,6 @@ if (restaurant.orderPizza) {
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
-/*
 ///////////////////////////////////////
 // Rest Pattern and Parameters
 // 1) Destructuring
