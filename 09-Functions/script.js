85fc63d8b35d6a249b8988761821846ecc9fc7fd
NikDoe <nikd0e@yandex.ru>
2021-05-16 16:58:35 +0300

    Functions Returning Functions

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -63,7 +63,6 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
-*/
 
 ///////////////////////////////////////
 // Functions Accepting Callback Functions
@@ -93,3 +92,23 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+///////////////////////////////////////
+// Functions Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
