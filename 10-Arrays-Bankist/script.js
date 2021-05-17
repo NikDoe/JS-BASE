a107f8fd8126c24fd9010f882756eb30c082f9f4
NikDoe <nikd0e@yandex.ru>
2021-05-17 21:08:36 +0300

    forEach With Maps and Sets

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -107,7 +107,6 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - '));
-*/
 
 ///////////////////////////////////////
 // Looping Arrays: forEach
@@ -134,3 +133,24 @@ movements.forEach(function (mov, i, arr) {
 // 1: function(450)
 // 2: function(400)
 // ...
+*/
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
