3c7e5b677fc6d9326ffaa67d35d6f5cae5d898fd
NikDoe <nikd0e@yandex.ru>
2021-05-23 18:28:51 +0300

    The reduce Method

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -266,7 +266,6 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
-*/
 
 ///////////////////////////////////////
 // The filter Method
@@ -284,3 +283,28 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+*/
+
+///////////////////////////////////////
+// The reduce Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
