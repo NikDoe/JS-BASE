07e11b7d083e0acdf66f9a9af42126d7d9671a9a
NikDoe <nikd0e@yandex.ru>
2021-05-24 20:02:42 +0300

    some method

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -521,3 +521,18 @@ console.log(account);
 
 // for (const ac of accounts) if (ac.owner === 'Jessica Davis') console.log(ac);
 */
+
+///////////////////////////////////////
+// some and every
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+// console.log(movements.some(mov => mov === -130)); // same includes method
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
