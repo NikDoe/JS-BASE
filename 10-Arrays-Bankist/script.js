5a49d8d313f0684234cbd56ca2f62b7c036ba9ab
NikDoe <nikd0e@yandex.ru>
2021-05-24 20:13:54 +0300

    every method

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -551,3 +551,13 @@ console.log(movements.includes(-130));
 
 const anyDeposits = movements.some(mov => mov > 0);
 console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
