8827fe7cb85c30d73ab35d59fc70305e0662cd21
NikDoe <nikd0e@yandex.ru>
2021-05-27 19:55:42 +0300

    Timers(setTimeout)

--- 11-Numbers-Dates-Timers-Bankist/script.js
+++ 11-Numbers-Dates-Timers-Bankist/script.js
@@ -526,3 +526,17 @@ console.log(
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
 */
+
+///////////////////////////////////////
+// Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
