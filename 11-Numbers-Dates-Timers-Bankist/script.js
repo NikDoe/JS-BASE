7b5db7033905818a82d06f2c3818fca5154aaebf
NikDoe <nikd0e@yandex.ru>
2021-05-27 19:09:32 +0300

    Internationalizing Numbers (Intl)

--- 11-Numbers-Dates-Timers-Bankist/script.js
+++ 11-Numbers-Dates-Timers-Bankist/script.js
@@ -498,3 +498,22 @@ const calcDaysPassed = (date1, date2) =>
 const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
 */
+
+///////////////////////////////////////
+// Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
