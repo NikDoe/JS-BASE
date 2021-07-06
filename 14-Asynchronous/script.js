0dc782367b6a9b96b15bff033545f32d9505ae19
NikDoe <nikd0e@yandex.ru>
2021-07-06 21:47:08 +0300

    Other Promise Combinators: race, allSettled and any

--- 14-Asynchronous/script.js
+++ 14-Asynchronous/script.js
@@ -556,7 +556,6 @@ console.log('1: Will get location');
   }
   console.log('3: Finished getting location');
 })();
-*/
 
 ///////////////////////////////////////
 // Running Promises in Parallel
@@ -584,3 +583,57 @@ const get3Countries = async function (c1, c2, c3) {
   }
 };
 get3Countries('portugal', 'canada', 'tanzania');
+*/
+
+///////////////////////////////////////
+// Other Promise Combinators: race, allSettled and any
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+// the first
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled no matter resolve or rejected, the all done
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+//if one of them rej, another will't done
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [ES2021] ingore rejected, only first fullfiled...only if all of them reject
+Promise.any([
+  Promise.reject('ERROR'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
