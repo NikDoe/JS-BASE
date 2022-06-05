f3075ced3b1ebc4d1cd225d047e61a14e6f6e5c2
NikDoe <nikd0e@yandex.ru>
2022-06-05 17:06:09 +0300

    variables assigment, camelcase & const in uppercase

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+//1
+let someName = 'Джон', admin;
+admin = someName;
+alert(admin);
+
+//2
+let ourPlanetName = "Земля";
+let currentUserName = "Джон";
+
+//3
+const BIRTHDAY = '18.04.1982'; //можно писать в верхнем регистре, так как это жостко закодированная переменная
+const age = someCode(BIRTHDAY); //переменная вычисляется лучше писать в нижнем регистре
